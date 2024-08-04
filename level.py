e02d74bd2b32884c1dbdc66ea8126ac9380d8dce
vassi <asrtjlbv@ffgh.com>
2024-08-04 14:03:49 +0300

    xrdftfg

--- level.py
+++ level.py
@@ -5,18 +5,26 @@ class Level():
         p= balk.Balk(0, 48)
         self.image_balk = p.beam_building()
         self.list=[]
+        self.list_image=[]
         self.level_maker()
 
     def sozdavatel(self):
-        for l in self.list:
-            pygame.display.get_surface().blit(self.image_balk, [l.left, l.top])
-            pygame.draw.rect(pygame.display.get_surface(),'#ffffff',l,1)
+        for l in range(len(self.list)):
+            pygame.display.get_surface().blit(self.list_image[l], [self.list[l].left, self.list[l].top])
+            pygame.draw.rect(pygame.display.get_surface(),'#ffffff',self.list[l],1)
     def level_maker(self):
-        for a in range(3):
-            l=pygame.rect.Rect(50,650-(self.image_balk.get_height()+50)*a,
-                               self.image_balk.get_width(),self.image_balk.get_height())
+        for a in range(5):
+            image_balk=self.image_balk
+            if a!=0 and a%2>0:
+                image_balk = pygame.transform.rotate(self.image_balk,-2)
+            elif a!=0 and a%2==0:
+                image_balk = pygame.transform.rotate(self.image_balk, 2)
+
+            l=pygame.rect.Rect(50,650-(image_balk.get_height()+50)*a,
+                               image_balk.get_width(),image_balk.get_height())
             self.list.append(l)
-            print(l)
+            self.list_image.append(image_balk)
+
 
 
 
